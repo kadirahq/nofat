131c721408e3a059fc8568e619a6b4859a11af60
Muhammed Thanish <mnmtanish@gmail.com>
2016-02-15 09:55:23 +0530

    Log command output

--- tasks/test.js
+++ tasks/test.js
@@ -1,14 +1,7 @@
 var path = require('path');
 var sh = require('shelljs');
-var build = require('./build');
 
 module.exports = function (args, srcd, libd) {
-  build([], srcd, libd);
-
   sh.cd(__rootdir);
-  sh.exec('mocha ' +
-    '--require babel-polyfill ' +
-    path.join(libd, '**/__tests__/**/*.js') + ' ' +
-    args.join(' ')
-  );
+  console.log(sh.exec('mocha --require babel-polyfill ' + path.join(libd, '**/__tests__/**/*.js') + ' ' + args.join(' ')));
 };
