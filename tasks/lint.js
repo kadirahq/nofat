efe4b5f2746aa3823bb75c5ac1ebe18ae6e6d925
Muhammed Thanish <mnmtanish@gmail.com>
2016-02-15 11:02:41 +0530

    Print command outputs by default

--- tasks/lint.js
+++ tasks/lint.js
@@ -2,6 +2,5 @@ var sh = require('shelljs');
 
 module.exports = function (args, srcd, libd) {
   sh.cd(__rootdir);
-  var res = sh.exec('eslint -c ./.eslintrc ' + srcd + ' ' + args.join(' '));
-  console.log(res.output);
+  sh.exec('eslint -c ./.eslintrc ' + srcd + ' ' + args.join(' '));
 };
