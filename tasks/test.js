1d2a68bda65cf943a9bed44af50217f59756b0e1
Muhammed Thanish <mnmtanish@gmail.com>
2016-02-15 10:04:33 +0530

    Fix output

--- tasks/test.js
+++ tasks/test.js
@@ -3,5 +3,6 @@ var sh = require('shelljs');
 
 module.exports = function (args, srcd, libd) {
   sh.cd(__rootdir);
-  console.log(sh.exec('mocha --require babel-polyfill ' + path.join(libd, '**/__tests__/**/*.js') + ' ' + args.join(' ')));
+  var res = sh.exec('mocha --require babel-polyfill ' + path.join(libd, '**/__tests__/**/*.js') + ' ' + args.join(' '));
+  console.log(res.output);
 };
